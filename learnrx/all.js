85439b457d53b7c35ca925360154880cf6971062
fffweb <fffweb@hotmail.com>
2017-04-27 16:52:11 +0800

    // Exercise 26: Converting from Arrays to Deeper Trees ok

--- learnrx/all.js
+++ learnrx/all.js
@@ -230,6 +230,19 @@ var ex20 = function () {
     //	 {"id": 70111470,"title": "Die Hard","boxart":"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" }
     // ];
 
+    var result = movieLists.
+        concatMap(function (movieList) {
+            return movieList.videos.concatMap(function (video) {
+                return video.boxarts.reduce(function (acc, curr) {
+                    if (acc.width * acc.length > curr.width * curr.length) {
+                        return curr;
+                    } else {
+                        return acc;
+                    }
+                })
+            })
+        });
+
     return movieLists.
         concatMap(function (movieList) {
             return movieList.videos.concatMap(function (video) {
@@ -448,16 +461,236 @@ console.log(ex20());
 
     //------------ COMPLETE THIS EXPRESSION --------------
     // Exercise 24: Retrieve each video's id, title, middle interesting moment time, and smallest box art url.
-    return movieLists.
+    var result = movieLists.
         concatMap(function (movieList) {
-                return movieList.videos.concatMap(function(movie){
-                    return movie.reduce(function(){
-
+            return movieList.videos.concatMap(function (video) {
+                return video.boxarts.reduce(function (acc, cur) {
+                    if (acc.width * acc.height < cur.width * cur.height) {
+                        return acc;
+                    } else {
+                        return cur;
+                    }
+                }).map(function (boxart) {
+                    return video.interestingMoments.reduce(function (acc, cur) {
+                        if (cur.type == 'Middle') {
+                            return cur;
+                        } else {
+                            return acc;
+                        }
+                    }).map(function (interestingMoment) {
+                        return { id: video.id, title: video.title, time: interestingMoment.time, url: boxart.url }
                     })
+
                 })
+            })
         });
 
-}
-)()
+    console.log(result);
 
+    // Exercise 24: Retrieve each video's id, title, middle interesting moment time, and smallest box art url.
+    var result1 = movieLists.concatMap(function (movieList) {
+        return movieList.videos.concatMap(function (video) {
+            return Array.zip(
+                video.boxarts.reduce(function (acc, curr) {
+                    if (acc.width * acc.height < curr.width * curr.height) {
+                        return acc;
+                    }
+                    else {
+                        return curr;
+                    }
+                }),
+                video.interestingMoments.filter(function (interestingMoment) {
+                    return interestingMoment.type === "Middle";
+                }),
+                function (boxart, interestingMoment) {
+                    return { id: video.id, title: video.title, time: interestingMoment.time, url: boxart.url };
+                });
+        })
+    });
+
+    console.log(result1);
+    console.log(result == result1);
+    return result;
+}
+)();
+
+// Exercise 25: Converting from Arrays to Trees
+(function () {
+    var lists = [
+        {
+            "id": 5434364,
+            "name": "New Releases"
+        },
+        {
+            "id": 65456475,
+            "name": "Thrillers"
+        }
+    ],
+        videos = [
+            {
+                "listId": 5434364,
+                "id": 65432445,
+                "title": "The Chamber"
+            },
+            {
+                "listId": 5434364,
+                "id": 675465,
+                "title": "Fracture"
+            },
+            {
+                "listId": 65456475,
+                "id": 70111470,
+                "title": "Die Hard"
+            },
+            {
+                "listId": 65456475,
+                "id": 654356453,
+                "title": "Bad Boys"
+            }
+        ];
+    // Exercise 25: Converting from Arrays to Trees
+
+    var result = lists.map(function (list) {
+        return {
+            name: list.name,
+            videos:
+            videos.filter(function (video) {
+                return video.listId === list.id;
+            }).map(function (video) {
+                return { id: video.id, title: video.title }
+            })
+        }
+    })
+
+    console.log(result);
+
+    return result;
+})();
+
+// Exercise 26: Converting from Arrays to Deeper Trees
+(function () {
+    var lists = [
+        {
+            "id": 5434364,
+            "name": "New Releases"
+        },
+        {
+            "id": 65456475,
+            name: "Thrillers"
+        }
+    ],
+        videos = [
+            {
+                "listId": 5434364,
+                "id": 65432445,
+                "title": "The Chamber"
+            },
+            {
+                "listId": 5434364,
+                "id": 675465,
+                "title": "Fracture"
+            },
+            {
+                "listId": 65456475,
+                "id": 70111470,
+                "title": "Die Hard"
+            },
+            {
+                "listId": 65456475,
+                "id": 654356453,
+                "title": "Bad Boys"
+            }
+        ],
+        boxarts = [
+            { videoId: 65432445, width: 130, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
+            { videoId: 65432445, width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" },
+            { videoId: 675465, width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+            { videoId: 675465, width: 120, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+            { videoId: 675465, width: 300, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
+            { videoId: 70111470, width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+            { videoId: 70111470, width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" },
+            { videoId: 654356453, width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+            { videoId: 654356453, width: 140, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" }
+        ],
+        bookmarks = [
+            { videoId: 65432445, time: 32432 },
+            { videoId: 675465, time: 3534543 },
+            { videoId: 70111470, time: 645243 },
+            { videoId: 654356453, time: 984934 }
+        ];
+
+    // 
+    var result_with_subarray = lists.map(function (list) {
+        return {
+            name: list.name,
+            videos: videos.filter(function (video) {
+                if (video.listId == list.id) {
+                    return video;
+                }
+            }).map(function (video) {
+                return {
+                    id: video.id,
+                    title: video.title,
+                    time: bookmarks.filter(function (bookmark) {
+                        if (bookmark.videoId === video.id) {
+                            return bookmark.time;
+                        }
+                    }),
+                    url: boxarts.filter(function (boxart) {
+                        if (boxart.videoId === video.id) {
+                            return boxart;
+                        }
+                    }).reduce(function (acc, curr) {
+                        if (acc.height * acc.width < curr.height * curr.width) {
+                            return acc;
+                        } else {
+                            return curr;
+                        }
+                    }).map(function (boxart) {
+                        return boxart.url;
+                    })
+                }
+            })
+        }
+    });
+
+    var result = lists.map(function (list) {
+        return {
+            name: list.name,
+            videos: videos.filter(function (video) {
+                if (video.listId == list.id) {
+                    return video;
+                }
+            }).concatMap(function (video) {
+                return Array.zip(
+                    boxarts.filter(function (boxart) {
+                        if (boxart.videoId === video.id) {
+                            return boxart;
+                        }
+                    }).reduce(function (acc, cuur) {
+                        if (acc.height * acc.width < cuur.height * cuur.width) {
+                            return acc;
+                        } else {
+                            return cuur;
+                        }
+
+                    }),
+                    bookmarks.filter(function (bookmark) {
+                        if (bookmark.videoId === video.id) {
+                            return bookmark;
+                        }
+                    }),
+                    function (boxart, bookmark) {
+                        return { id: video.id, title: video.title, time: bookmark.time, url: boxart.url }
+                    }
+                )
+            })
+        }
+    });
+    console.log(result);
+
+    return result;
+
+}
+)();
 // Exercise 42: Retrying after errors
