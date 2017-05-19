ec2a006a065a70348360b17e677bd7cc3671182f
fffweb <fffweb@hotmail.com>
2017-05-19 16:47:24 +0800

    ts query jsonp can work, but sina response is not a jsonp, need to use IO.script

---
+++ tsjquery/app/app.ts
@@ -0,0 +1,62 @@
+import { Observable, Observer } from 'rxjs/Rx';
+import * as $ from 'jquery';
+var url_sina = "http://hq.sinajs.cn/?list=BU1709,RU1709,I1709";
+$(document).ready(() => {
+    jsonp(url_sina, function (data) {
+        alert(data);
+    });
+})
+
+function get(url) {
+    return Observable.create(function (observer: Observer<String>) {
+        // Make a traditional Ajax request
+        var req = new XMLHttpRequest();
+        req.open('GET', url);
+        req.onload = function () {
+            if (req.status == 200) {
+                // If the status is 200, meaning there have been no problems,
+                // Yield the result to listeners and complete the sequence
+                observer.next(req.response);
+                observer.complete();
+            }
+            else {
+                // Otherwise, signal to listeners that there has been an error
+                observer.error(new Error(req.statusText));
+            }
+        };
+        req.onerror = function () {
+            observer.error(new Error("Unknown Error"));
+        };
+        req.send();
+    });
+}
+// Create an Ajax Observable
+// var test = get(url);
+// test.subscribe(
+//     function next(x) { console.log('Result: ' + x); },
+//     function error(err) { console.log('Error: ' + err); },
+//     function complete() { console.log('Completed'); }
+// );
+
+// Observable.interval(1000).mergeMap(x => {
+//     return get(url)
+// }).subscribe(
+//     function next(x) { console.log('Result: ' + x); },
+//     function error(err) { console.log('Error: ' + err); },
+//     function complete() { console.log('Completed'); }
+//     );
+
+function jsonp(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    window[callbackName] = function (data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    var script = document.createElement('script');
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+    // console
+}
+
